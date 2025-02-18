05875001e9696dafc772a7ce4f84d65b8d02d735
Sergi0ZM <sergioazm98@gmail.com>
2025-02-17 19:41:17 -0500

    Descripción y hobby

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola Mundo");
+        System.out.println("SERGIO ZAMORA");
+        System.out.println("Me gusta leer");
+        System.out.println("Bruno");
 
-        System.out.println("Comandos git");
 
-        System.out.println("COMANDOS GIT");
-        System.out.println("git status");
-        System.out.println("git add .");
-        System.out.println("git commit -m ");
-        System.out.println("git push origin nombrerama");
-        System.out.println("git branch");
-        System.out.println("git switch");
     }
 }
