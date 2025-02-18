3f33dce2ee1c1b69dd5479be093868726f803613
Daniel Valencia <danielvalenbf@gmail.com>
2025-02-17 19:37:15 -0500

    🚀Feat: Datos Personales

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,5 +14,9 @@ public class Main {
         System.out.println("git push origin nombrerama");
         System.out.println("git branch");
         System.out.println("git switch");
+
+        System.out.println("DANIEL ALEJANDRO VALENCIA");
+        System.out.println("👨🏻‍💻PROGRAMAR Y ESCUCHAR MUSICA🎵");
+        System.out.println("🐈 GATO - SABATH");
     }
 }
