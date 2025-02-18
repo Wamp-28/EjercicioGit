74859408def790c57b33366e475bd893d0c8d1bd
DFLH <danielleon2520@gmail.com>
2025-02-17 19:35:20 -0500

    Datos Personales

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,5 +14,11 @@ public class Main {
         System.out.println("git push origin nombrerama");
         System.out.println("git branch");
         System.out.println("git switch");
+
+        System.out.println("Daniel Felipe Leon Higuera");
+        System.out.println("Dormir");
+        System.out.println("Perro-Lucas");
+
+
     }
 }
