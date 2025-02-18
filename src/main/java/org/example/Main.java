e88ed351ef1992d57546ab0373cd25b55c5607da
Chamy22-max <kamyprada22@gmail.com>
2025-02-17 19:37:54 -0500

    comandos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,5 +14,9 @@ public class Main {
         System.out.println("git push origin nombrerama");
         System.out.println("git branch");
         System.out.println("git switch");
+
+        System.out.println("Camila Andrea Prada Vargas");
+        System.out.println("Caminar");
+        System.out.println("perro - danu");
     }
 }
